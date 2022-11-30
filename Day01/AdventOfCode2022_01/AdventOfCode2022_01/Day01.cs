9fb8b00b82cafe2760930f7ca712163c56509287
Jonas Kubesch <jonas.kubesch@gmail.com>
2022-11-30 20:43:09 +0100

    AOC22-01 Init

---
+++ Day01/AdventOfCode2022_01/AdventOfCode2022_01/Day01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+public class Day01
+{
+    private static string INPUT_PATH = "D:\\Studium\\Semester5\\AdventOfCode\\AdventOfCode2022\\Day01\\AdventOfCode2022_01\\AdventOfCode2022_01\\input.txt";
+
+    public static void Main()
+    {
+        string input;
+        string[] inputLines;
+        using (StreamReader streamReader = new StreamReader(INPUT_PATH))
+        {
+            input = streamReader.ReadToEnd();
+        }
+
+        inputLines = input.Split("\r\n");
+
+        SanityCheck(inputLines);
+    }
+
+    private static void SanityCheck(string[] inputLines)
+    {
+        Console.WriteLine($"{inputLines.Length} lines read, first entry is {inputLines[0]}, last entry is {inputLines[inputLines.Length - 1]}");
+    }
+}
