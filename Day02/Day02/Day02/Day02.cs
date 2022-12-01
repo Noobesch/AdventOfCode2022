ea29e1d8886a2c01464b8c93dedd0d539d83f431
Jonas Kubesch <jonas.kubesch@gmail.com>
2022-12-01 07:04:49 +0100

    AOC22-02 Prepare day 2

---
+++ Day02/Day02/Day02/Day02.cs
@@ -0,0 +1,37 @@
+﻿public class Day02
+{
+    private static string REAL_INPUT_PATH = "..\\..\\..\\realInput.txt";
+    private static string SAMPLE_INPUT_PATH = "..\\..\\..\\testInput.txt";
+
+    public static void Main()
+    {
+        string input;
+        
+        using (StreamReader streamReader = new StreamReader(REAL_INPUT_PATH))
+        {
+            input = streamReader.ReadToEnd();
+        }
+
+        string[] inputLines = input.Split("\r\n");
+        SanityCheck(inputLines);
+
+
+        AnswerPart1();
+        AnswerPart2();
+    }
+
+    private static void SanityCheck(string[] inputLines)
+    {
+        Console.WriteLine($"{inputLines.Length} lines read, first entry is {inputLines[0]}, last entry is {inputLines[^2]}");
+    }
+
+    private static void AnswerPart1()
+    {
+        Console.WriteLine($"Answer part 1: ");
+    }
+
+    private static void AnswerPart2()
+    {
+        Console.WriteLine($"Answer part 2: ");
+    }
+}
