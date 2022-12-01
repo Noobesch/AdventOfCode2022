57def0d061c32a35d9508f7ff0fad816e797c2cc
Jonas Kubesch <jonas.kubesch@gmail.com>
2022-12-01 06:43:21 +0100

    AOC22-1 Part 1

--- Day01/AdventOfCode2022_01/AdventOfCode2022_01/Day01.cs
+++ Day01/AdventOfCode2022_01/AdventOfCode2022_01/Day01.cs
@@ -1,20 +1,43 @@
 ﻿using System;
+using System.Linq;
 
 
 public class Day01
 {
-    private static string INPUT_PATH = "D:\\Studium\\Semester5\\AdventOfCode\\AdventOfCode2022\\Day01\\AdventOfCode2022_01\\AdventOfCode2022_01\\input.txt";
+    private static string INPUT_PATH1 = "D:\\Studium\\Semester5\\AdventOfCode\\AdventOfCode2022\\Day01\\AdventOfCode2022_01\\AdventOfCode2022_01\\input1.txt";
+    private static string INPUT_PATH2 = "D:\\Studium\\Semester5\\AdventOfCode\\AdventOfCode2022\\Day01\\AdventOfCode2022_01\\AdventOfCode2022_01\\input2.txt";
 
     public static void Main()
     {
         string input;
         string[] inputLines;
-        using (StreamReader streamReader = new StreamReader(INPUT_PATH))
+        List<int> calorieList = new List<int>();
+        using (StreamReader streamReader = new StreamReader(INPUT_PATH1))
         {
             input = streamReader.ReadToEnd();
         }
 
         inputLines = input.Split("\r\n");
+
+        int currentElfCalories = 0;
+
+        foreach(var line in inputLines)
+        {
+            if(line == "")
+            {
+                calorieList.Add(currentElfCalories);
+                currentElfCalories = 0;
+            }
+            else
+            {
+                currentElfCalories += int.Parse(line);
+            }
+        }
+
+        calorieList.Sort();
+
+        AnswerPart1(calorieList);
+        
 
         SanityCheck(inputLines);
     }
@@ -23,4 +46,10 @@ public class Day01
     {
         Console.WriteLine($"{inputLines.Length} lines read, first entry is {inputLines[0]}, last entry is {inputLines[inputLines.Length - 1]}");
     }
+
+    private static void AnswerPart1(List<int> calorieList)
+    {
+        Console.WriteLine($"The lowest value ist {calorieList[0]}, highest is {calorieList[calorieList.Count - 1]}");
+    }
+
 }
