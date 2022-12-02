c63529ccea183e3b12c9e2676f84be6ff06bed88
Jonas Kubesch <jonas.kubesch@gmail.com>
2022-12-02 07:16:13 +0100

    AOC22-02 Task 1

--- Day02/Day02/Day02/Day02.cs
+++ Day02/Day02/Day02/Day02.cs
@@ -1,5 +1,15 @@
-﻿public class Day02
+﻿using System.Diagnostics;
+
+public enum RockPaperScissorsState
 {
+    Rock,
+    Paper,
+    Scissors
+}
+
+public class Day02
+{
+
     private static string REAL_INPUT_PATH = "..\\..\\..\\realInput.txt";
     private static string SAMPLE_INPUT_PATH = "..\\..\\..\\testInput.txt";
 
@@ -12,11 +22,38 @@
             input = streamReader.ReadToEnd();
         }
 
+
         string[] inputLines = input.Split("\r\n");
         SanityCheck(inputLines);
+        
+        (RockPaperScissorsState opponent, RockPaperScissorsState player)[] rounds = new (RockPaperScissorsState opponent, RockPaperScissorsState player)[inputLines.Length];
+        
+        for (long lineIndex = 0; lineIndex < inputLines.Length; lineIndex++)
+        {
+            string[] lineContent = inputLines[lineIndex].Split(" ");
+
+            RockPaperScissorsState opponent = lineContent[0] switch
+            {
+                "A" => RockPaperScissorsState.Rock,
+                "B" => RockPaperScissorsState.Paper,
+                "C" => RockPaperScissorsState.Scissors,
+                _ => throw new ArgumentException()
+            };
+
+            RockPaperScissorsState player = lineContent[1] switch
+            {
+                "X" => RockPaperScissorsState.Rock,
+                "Y" => RockPaperScissorsState.Paper,
+                "Z" => RockPaperScissorsState.Scissors,
+                _ => throw new ArgumentException()
+            };
+
+            rounds[lineIndex] = (opponent, player);
+        }
 
 
-        AnswerPart1();
+
+        AnswerPart1(rounds);
         AnswerPart2();
     }
 
@@ -25,9 +62,31 @@
         Console.WriteLine($"{inputLines.Length} lines read, first entry is {inputLines[0]}, last entry is {inputLines[^2]}");
     }
 
-    private static void AnswerPart1()
+    private static void AnswerPart1((RockPaperScissorsState opponent, RockPaperScissorsState player)[] rounds)
     {
-        Console.WriteLine($"Answer part 1: ");
+        long points = 0;
+        foreach (var round in rounds)
+        {
+            if (round.opponent == RockPaperScissorsState.Rock && round.player == RockPaperScissorsState.Paper ||
+                round.opponent == RockPaperScissorsState.Paper && round.player == RockPaperScissorsState.Scissors ||
+                round.opponent == RockPaperScissorsState.Scissors && round.player == RockPaperScissorsState.Rock)
+            {
+                points += 6;
+            }
+            else if (round.opponent == round.player)
+            {
+                points += 3;
+            }
+
+            points += round.player switch
+            {
+                RockPaperScissorsState.Rock => 1,
+                RockPaperScissorsState.Paper => 2,
+                RockPaperScissorsState.Scissors => 3
+            };
+        }
+
+        Console.WriteLine($"Answer part 1: The score is {points}");
     }
 
     private static void AnswerPart2()
