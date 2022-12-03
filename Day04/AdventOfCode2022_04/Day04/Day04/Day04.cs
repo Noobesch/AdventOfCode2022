6521b6e359d5483726bf9b818f0b2b0716bcbf89
Jonas Privat <jonas.kubesch@gmail.com>
2022-12-03 11:13:59 +0100

    AOC22-04 Prepare day 4

---
+++ Day04/AdventOfCode2022_04/Day04/Day04/Day04.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Day03
+{
+
+    private static string REAL_INPUT_PATH = "..\\..\\..\\realInput.txt";
+    private static string SAMPLE_INPUT_PATH = "..\\..\\..\\testInput.txt";
+
+    public static void Main()
+    {
+        string input;
+
+        using (StreamReader streamReader = new StreamReader(REAL_INPUT_PATH))
+        {
+            input = streamReader.ReadToEnd();
+
+            while (!streamReader.EndOfStream)
+            {
+            }
+        }
+
+        string[] inputLines = input.Split("\r\n");
+        SanityCheck(inputLines);
+
+        AnswerPart1();
+        AnswerPart2();
+    }
+
+    private static void SanityCheck(string[] inputLines)
+    {
+        Console.WriteLine($"{inputLines.Length} lines read, first entry is {inputLines[0]}, last entry is {inputLines[^2]}");
+    }
+
+    private static void AnswerPart1()
+    {
+        long score = 0;
+
+        Console.WriteLine($"Answer part 1: The score is {score}");
+    }
+
+    private static void AnswerPart2()
+    {
+        long score = 0;
+
+        Console.WriteLine($"Answer part 2: The score is {score}");
+    }
+}
