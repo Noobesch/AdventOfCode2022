af1ab7d0e4b1199cfabb3147638eef9026ef6218
Jonas Kubesch <jonas.kubesch@gmail.com>
2022-12-07 08:16:28 +0100

    AOC22-07 Tree try task 1

--- Day07/Day07/Day07/Day06.cs
+++ Day07/Day07/Day07/Day06.cs
@@ -1,52 +1,97 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 public class Node
 {
-    public string name;
-    public string collectiveSize;
-    public Node parent;
-    public List<(string id, Node node)> children = new List<(string, Node)>();
-    public string ID
+    public string Name;
+    public List<Node> Children;
+    public Node Parent;
+    public long Size;
+    public bool IsDir;
+    public int identifier;
+    public static int ID = 0;
+
+    public Node(string name)
     {
-        get => collectiveSize + name;
+        Name = name;
+        Children = new List<Node>();
+        identifier = ID++;
+    }
+
+
+    public void AddChild(Node child)
+    {
+        Children.Add(child);
+        child.Parent = this;
+    }
+
+    public Node FindChild(string name)
+    {
+        return Children.Find(node => node.Name == name);
+    }
+
+
+    public override string ToString()
+    {
+        return Name + " " + GetSize();
+    }
+
+    public long GetSize()
+    {
+        long size = Size;
+
+        foreach (var child in Children)
+        {
+            size += child.GetSize();
+        }
+
+        return size;
     }
 }
 
 public class Tree
 {
-    public Node root = null;
+    public Node Root;
+    public Node CurrentNode;
+    public List<Node> ContainedNodes = new List<Node>();
 
-    public void AddAsChild(Node parent, Node child)
+    public Tree()
     {
-        FindInChildren(parent, root).children.Add((child.ID, child));
+        Root = new Node("/");
+        CurrentNode = Root;
     }
 
-    public Node FindInChildren(Node nodeToFind, Node startNode)
+    public void AddNode(Node node)
     {
-        if (startNode == null)
+        if (Root == null)
         {
-            return null;
-        }
-
-        Node foundNode = startNode.children.Find(child => child.id == nodeToFind.ID).node;
-
-        if (foundNode != null)
-        {
-            return foundNode;
+            Root = node;
         }
         else
         {
-            foreach (var tuple in startNode.children)
-            {
-                foundNode = FindInChildren(nodeToFind, tuple.node);
-                if (foundNode != null)
-                {
-                    return foundNode;
-                }
-            }
+            CurrentNode.AddChild(node);
         }
 
-        return null;
+        ContainedNodes.Add(node);
+    }
+
+    public void ChangeActiveNode(string name)
+    {
+        if (name == "..")
+        {
+            CurrentNode = CurrentNode.Parent;
+        }
+        else if (CurrentNode.Name != name)
+        {
+            CurrentNode = CurrentNode.FindChild(name);
+        }
+
+    }
+
+    public Node FindContainedNode(string name)
+    {
+        return ContainedNodes.Find(node => node.Name == name);
     }
 }
 
@@ -55,14 +100,12 @@ public class Day07
 
     private static string REAL_INPUT_PATH = "..\\..\\..\\realInput.txt";
     private static string SAMPLE_INPUT_PATH = "..\\..\\..\\testInput.txt";
-    private static Dictionary<string, List<(string size, string name)>> sizeDict = new Dictionary<string, List<(string, string)>>();
     private const int THRESHOLD = 100000;
 
 
     public static void Main()
     {
-        //Sample Entry, D, new List() {(123, b.text), (345, c.text), (dir, d)
-        List<(string size, string name)> activeList = new List<(string size, string name)>();
+        Tree tree = new Tree();
 
         using (StreamReader streamReader = new StreamReader(REAL_INPUT_PATH))
         {
@@ -71,58 +114,41 @@ public class Day07
             {
                 line = streamReader.ReadLine().Replace("\r\n", "");
 
+                Console.WriteLine(line);
 
                 if (line.Contains("$ cd "))
                 {
-                    var directory = line.Replace("$ cd ", "");
-
-                    if (directory == "..")
-                    {
-                        continue;
-                    }
-
-                    if (sizeDict.ContainsKey(directory))
-                    {
-                        activeList = sizeDict[directory];
-                        continue;
-                    }
-
-                    sizeDict.Add(directory, new List<(string size, string name)>());
-                    activeList = sizeDict[directory];
+                    string name = line.Replace("$ cd ", "");
+                    tree.ChangeActiveNode(name);
                 }
                 else if (!line.Contains("$ ls"))
                 {
-                    var stringArray = line.Split(' ');
-                    activeList.Add((stringArray[0], stringArray[1]));
-                }
-            }
-        }
+                    string[] information = line.Split(" ");
+                    string name = information[1];
+                    bool isLeaf = long.TryParse(information[0], out long size);
 
-        while (!DictionaryIsFinished())
-        {
-            Dictionary<string, List<(string size, string name)>> tempDict = new Dictionary<string, List<(string, string)>>();
-            foreach (var entry in sizeDict)
-            {
-                List<(string size, string name)> tempEntry = new List<(string, string)>();
-                tempEntry.AddRange(entry.Value);
+                    Node newNode = new Node(name);
 
-
-                foreach (var tuple in entry.Value)
-                {
-                    if (tuple.size == "dir")
+                    if (isLeaf)
                     {
-                        tempEntry.Remove(tuple);
-                        tempEntry.Add((SumEntryAt(tuple.name), tuple.name));
+                        newNode.Size = size;
                     }
+                    else
+                    {
+                        newNode.IsDir = true;
+                    }
+
+                    if (line == "dir pcccp")
+                    {
+
+                    }
+                    tree.AddNode(newNode);
                 }
-
-                tempDict.Add(entry.Key, tempEntry);
             }
-
-            sizeDict = tempDict;
         }
 
-        AnswerPart1();
+
+        AnswerPart1(tree);
         AnswerPart2();
     }
 
@@ -131,19 +157,23 @@ public class Day07
         Console.WriteLine($"{inputLines.Length} lines read, first entry is {inputLines[0]}, last entry is {inputLines[^2]}");
     }
 
-    private static void AnswerPart1()
+    private static void AnswerPart1(Tree tree)
     {
         long sum = 0;
 
-        foreach (var entry in sizeDict)
-        {
-            long entrySum = int.Parse(SumEntryAt(entry.Key));
 
-            if (entrySum <= THRESHOLD)
+        foreach (var node in tree.ContainedNodes)
+        {
+            if (!node.IsDir)
             {
-                sum += entrySum;
+                continue;
             }
 
+            long size = node.GetSize();
+            if (size < THRESHOLD)
+            {
+                sum += size;
+            }
         }
         Console.WriteLine($"The first answer is {sum}");
     }
@@ -151,40 +181,5 @@ public class Day07
     private static void AnswerPart2()
     {
         Console.WriteLine($"The second answer is ");
-    }
-
-    private static bool DictionaryIsFinished()
-    {
-        foreach (var tupleList in sizeDict.Values)
-        {
-            bool containsDir = tupleList.Any(tuple => tuple.size == "dir");
-            if (containsDir)
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private static string SumEntryAt(string key)
-    {
-        var entries = sizeDict[key];
-
-        long sum = 0;
-
-        foreach (var entry in entries)
-        {
-            if (entry.size != "dir")
-            {
-                sum += int.Parse(entry.size);
-            }
-            else
-            {
-                return entry.size;
-            }
-        }
-
-        return sum.ToString();
     }
 }
